3a8e3be1dec20d29ff8e14ea96ad4c4236d49935
tomz123 <tomz12321@gmail.com>
2018-02-11 18:47:57 +0800

    Rework from Angular to React.

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      text: "",
+      name: "",
+    };
+
+    this.pokemonNameOnChange = this.pokemonNameOnChange.bind(this);
+    this.searchPokemon = this.searchPokemon.bind(this);
+  }
+
+  pokemonNameOnChange(event) {
+    //console.log(event.target)
+    this.setState({text: event.target.value});
+  }
+  
+  searchPokemon(event){
+    //console.log(event);
+    event.preventDefault();
+    //console.log(this.state.text);
+    const id = this.state.text;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw res.status;
+        }
+        return res.json();
+      })
+      .then((body) => {
+        this.setState({name: body.name});
+    })
+    .catch((err) => {
+      this.setState({err})
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      
+        <form onSubmit={this.searchPokemon}>
+          Enter Pokemon Name or National Pokedex number:
+          <input type="text" name="pokemonName" onChange={this.pokemonNameOnChange} required/>
+          <input type="submit" id="submit" value="Search"/>
+        </form>
+          <p>Name: {this.state.name}</p>
+          <p>{this.state.err}</p>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
